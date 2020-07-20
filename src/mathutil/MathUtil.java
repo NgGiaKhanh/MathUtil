ebb9e55112f260e3ca68595ff8b9666fb1e4db8d
NgGiaKhanh <khanh3oil48@gamil.com>
2020-07-20 15:08:35 +0700

    3rd: add JUnit Test class & jar file

--- src/mathutil/MathUtil.java
+++ src/mathutil/MathUtil.java
@@ -19,6 +19,40 @@ public class MathUtil {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("5! = " + computeFactorial(5));
+        // tôi muốn biết 5! chạy đúng ko
+        // vậy tôi in kết quả xử lí của hàm ra (actulal value)
+        // tôi so sánh nó coi có = 120 mà tôi tính trước ko(expected value)
+        // nếu hàm có 1 value tra ve (actual - gia su 120)
+        // va khop voi cai tui can no phai the(expected - gia su minh can ham tra ve 120)
+        // tra ve 120
+        // actual khop voi expected, ham chay nhu mong doi, ham chay dung
+        // testing chang qua la chay app, chay ham, xem ket qua tra ve co dung nhu mong doi hay ko
+        // actual va expected la nhu nhau
+        // neu ko khop, code co bug hoac mnh ky vong sai
+        
+        // expected 1
+        System.out.println("1! = " + computeFactorial(1));
+        
+         // expected 1
+        System.out.println("0! = " + computeFactorial(0));
+        
+        // cach test kieu nay  phai:
+        // > chuan data dau vao
+        // > tinh truoc ket qua tra ve (expected)
+        // > so sanh ket qua tra ve tu ham (actual) voi expected = mat
+        // tu dua ra ket luan -> luan ham = mat
+        
+        // expected: exception
+        System.out.println("-5! = " + computeFactorial(-5));
+        
+        //ki thuat dung mat doi chieu ket qua on NHUNG KO HIEU QUA
+        //ki thuat co the doi chieu ket qua duoc luon ma, may so duoc expected va actual
+        // GIANG HO DUA RA 1 BO THU VIEN, 1 FRAMEWORK GIUP MINH
+        // TU DONG SO SANH 2 KET QUA expected va actual
+        // FRAMEWORK nay se tu dong so sanh va dua ra mau sac de ham y rang
+        // ham chay dung ma xanh
+        // ham chay sai, ko nhu ky vong mau do
+        // ap dung cho 1 loat data dau vao
     }
 
 }
---
+++ test/tdd/MathUtilTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tdd;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MathUtilTest {
+    
+    //day la class db, chua cac ham ma moi ham dung de test cac ham ben class
+    // cua du an vi du test ham cF() coi chay dung ko
+    // moi ham se chay 1 hoac nhieu lenh cF() tuy ban de kiem tra
+    // khop mau xanh
+    // ko khop mau do
+    // moi ham se la 1 ham main() neu no mang phia truoc 1 ky hieu
+    // @test - goi la annotation
+    @Test
+    public void testSuccessfulCase() {
+        // ham nay goi la cF() ma tra ve ket qua giai thua
+        // assertEquals(120, 720);
+        
+        // expect 1, atual : cho tinh toan chay ham
+        assertEquals(1, khanh.util.MathUtil.computeFactorial(1));
+        
+        // expect 1, atual : cho tinh toan chay ham
+        assertEquals(1, khanh.util.MathUtil.computeFactorial(0));
+        
+        assertEquals(720, khanh.util.MathUtil.computeFactorial(6));
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testFailCase() {
+        // ham nay test nhung tinh huong ca chon
+        // nguoi dung dua 5!, ko tinh duoc
+        // ham cF() phai nem ve ngoai le, 1 tinh huong bat thuong, 1 exception
+        // nhung exception ko phai la value de assert()
+        // ta phai dung 1 chieu khac
+        khanh.util.MathUtil.computeFactorial(-5); // ko sai assertEquals()
+        
+        // dua -5 vao tham so, minh (dev tk ra ham cF() coi day la ca chon
+        // dap vao mat ng dua con so -5 nay vao
+        // bang cach nem ra ngoai le IllegalArgumentException
+    }
+    
+}
+// do: chi can 1 thang do, la coi nhu ca dam do, ham chay dung hau het
+// cac cases, nhung chi 1 tinh huong sai, coi nhu ham sai
+// xanh: tat ca phai xanh, ham y ham dung
